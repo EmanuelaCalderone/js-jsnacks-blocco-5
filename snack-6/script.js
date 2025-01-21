8bf0a8b60cac258db97d8f71c34387711931f94f
Emanuela Calderone <emanuelacalderone3006@gmail.com>
2025-01-21 15:45:26 +0100

    complete snacks 6-7

--- snack-6/script.js
+++ snack-6/script.js
@@ -12,3 +12,13 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+const min15 = zucchine.filter((zucchina) => {
+  return zucchina.length >= 15;
+});
+
+const below15 = zucchine.filter((zucchina) => {
+  return zucchina.length < 15;
+});
+
+console.log(min15, below15);
